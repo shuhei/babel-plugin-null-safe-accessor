8ee745693fd54bf170ebe31a2a51fba7e89afc48
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-12-06 11:15:01 +0900

    Fix tests on Node 0.12.

--- test/index.js
+++ test/index.js
@@ -1,43 +1,43 @@
-var test = require('tape');
+const test = require('tape');
 
 test('safe accessor for defined props', (t) => {
   t.plan(1);
-  var a = { b: { c: 123 } };
+  const a = { b: { c: 123 } };
 
   t.strictEqual(a?.b?.c, 123);
 });
 
 test('safe accessor for undefined props', (t) => {
   t.plan(1);
-  var b = {};
+  const b = {};
 
   t.strictEqual(b?.c?.d, undefined);
 });
 
 test('call for defined method', (t) => {
   t.plan(1);
-  var c = { d: { e(foo, bar) { return foo + bar } } };
+  const c = { d: { e(foo, bar) { return foo + bar } } };
 
   t.strictEqual(c?.d?.e(1, 23), 24);
 });
 
 test('call for null method', (t) => {
   t.plan(1);
-  var d = { e: null };
+  const d = { e: null };
 
   t.strictEqual(d?.e?.f(), null);
 });
 
 test('safe call for undefined method', (t) => {
   t.plan(1);
-  var b = {};
+  const b = {};
 
   t.strictEqual(b.c?(), undefined);
 });
 
 test('safe call for defined method', (t) => {
   t.plan(1);
-  var c = { d: { e(foo, bar) { return foo + bar } } };
+  const c = { d: { e(foo, bar) { return foo + bar } } };
 
   t.strictEqual(c?.d?.e?(1, 23), 24);
 });
